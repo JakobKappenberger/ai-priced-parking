75d7006eff5aaeba3d5241c281e12c545d598b28
JakobGutmann <JakobGutmann@users.noreply.github.com>
2021-06-21 13:13:07 +0200

    add checkpointing and more documentation of runs

--- src/experiment.py
+++ src/experiment.py
@@ -1,22 +1,31 @@
+import json
 from datetime import datetime
 from pathlib import Path
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from tensorforce.execution import Runner
+import seaborn as sns
+from cmcrameri import cm
 
 from custom_environment import CustomEnvironment
+from external.tensorforce.execution import Runner
+
+sns.set_style('dark')
+sns.set_context('paper')
 
 
 class Experiment:
     def __init__(self, agent: str,
                  num_episodes: int,
+                 args,
                  batch_agent_calls: bool = False,
                  sync_episodes: bool = False,
                  document: bool = True,
+                 adjust_free: bool = False,
                  num_parallel: int = 1,
-                 reward_key: str = 'occupancy'):
+                 reward_key: str = 'occupancy',
+                 checkpoint: str = None):
         """
         Class to run individual experiments.
         :param agent: Agent specification (Path to JSON-file)
@@ -29,13 +38,38 @@ class Experiment:
         self.num_episodes = num_episodes
         self.batch_agent_calls = batch_agent_calls
         self.sync_episodes = sync_episodes
-        self.timestamp = datetime.now().strftime('%y%m-%d-%H%M')
-        self.path = Path(".").absolute().parent / "Experiments" / self.timestamp
+        if checkpoint is not None:
+            self.resume_checkpoint = True
+            self.timestamp = checkpoint
+        else:
+            self.resume_checkpoint = False
+            self.timestamp = datetime.now().strftime('%y%m-%d-%H%M')
+        self.outpath = Path(".").absolute().parent / "Experiments" / self.timestamp
+        # Create directory (if it does not exist yet)
+        self.outpath.mkdir(parents=True, exist_ok=True)
         env_kwargs = {
             'timestamp': self.timestamp,
             'reward_key': reward_key,
-            'document': document
+            'document': document,
+            'adjust_free': adjust_free
         }
+
+        if self.resume_checkpoint:
+            agent = dict()
+            agent["directory"] = str(self.outpath / 'model-checkpoints')
+            agent["format"] = "checkpoint"
+        else:
+            # Load json file for agent init
+            with open(agent, 'r') as fp:
+                agent = json.load(fp=fp)
+            # Update checkpoint path
+            agent['saver']['directory'] = str(self.outpath / 'model-checkpoints')
+
+            # Document Config
+            args['agent'] = agent
+            with open(str(self.outpath / 'config.txt'), 'w') as outfile:
+                json.dump(args, outfile)
+
         # Create appropriate number of environments
         if num_parallel > 1:
             self.runner = Runner(agent=agent, environment=CustomEnvironment, remote='multiprocessing',
@@ -52,7 +86,7 @@ class Experiment:
         :return:
         """
         self.runner.run(num_episodes=self.num_episodes, batch_agent_calls=self.batch_agent_calls,
-                        sync_episodes=self.sync_episodes)
+                        sync_episodes=self.sync_episodes, save_best_agent=str(self.outpath / 'best_agent'))
 
         results_dict = dict()
         # Accessing the metrics from runner
@@ -61,22 +95,21 @@ class Experiment:
         mean_reward = rewards / episode_length
         metrics_df = pd.DataFrame.from_dict({'rewards': rewards, 'episode_length': episode_length,
                                              'mean_reward': mean_reward})
-        metrics_df.to_csv(str(self.path / 'result.csv'))
+        metrics_df.to_csv(str(self.outpath / 'result.csv'))
 
         # plotting mean-reward over episodes
-        fig, ax = plt.subplots(figsize=(20, 10))
-        ax.plot(range(len(mean_reward)), metrics_df.mean_reward, linewidth=3)
+        fig, ax = plt.subplots(figsize=(20, 10), constrained_layout=True)
+        ax.plot(range(len(mean_reward)), metrics_df.mean_reward, linewidth=5, color=cm.bamako(0))
         rolling_average = metrics_df.mean_reward.rolling(10).mean()
-        ax.plot(range(len(rolling_average)), rolling_average, linewidth=3, color="red")
-        # plt.xticks(fontsize=15)
-        ax.set_ylabel('Mean Reward', fontsize=22)
-        ax.set_xlabel('# Episodes', fontsize=22)
+        ax.plot(range(len(rolling_average)), rolling_average, linewidth=3, color=cm.bamako(1.0))
+        ax.set_ylabel('Mean Reward per Episode', fontsize=30)
+        ax.set_xlabel('# Episodes', fontsize=30)
         ax.grid(True)
-        ax.tick_params(axis="y", labelsize=15)
-        ax.tick_params(axis="x", labelsize=15)
+        ax.tick_params(axis="y", labelsize=25)
+        ax.tick_params(axis="x", labelsize=25)
 
         fig.savefig(
-            str(self.path / 'reward_plot.pdf'),
+            str(self.outpath / 'reward_plot.pdf'),
             dpi=300)
 
         # Close runner
