b402207f8f565295a746a6677b92902e83bbd7e3
JakobGutmann <JakobGutmann@users.noreply.github.com>
2021-06-23 16:18:09 +0200

    fix boolean args

--- src/run_experiments.py
+++ src/run_experiments.py
@@ -5,6 +5,14 @@ sys.path.append("./external")
 
 from experiment import Experiment
 
+
+def add_bool_arg(parser, name, default=False):
+    group = parser.add_mutually_exclusive_group(required=False)
+    group.add_argument('--' + name, dest=name, action='store_true')
+    group.add_argument('--no-' + name, dest=name, action='store_false')
+    parser.set_defaults(**{name: default})
+
+
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("-p", "--num_parallel", type=int,
@@ -14,18 +22,15 @@ if __name__ == "__main__":
     parser.add_argument('-n', '--episodes', type=int, help='Number of episodes')
     parser.add_argument('-r', '--reward_key', type=str, default='occupancy',
                         help='Reward function to use')
-    parser.add_argument('-b', '--batch_agent_calls', type=bool, default=False,
-                        help='Whether or not to call agent in batches')
-    parser.add_argument('-s', '--sync_episodes', type=bool, default=False,
-                        help='Whether or not to sync episodes when executing in parallel')
-    parser.add_argument('-d', '--document', type=bool, default=True,
-                        help='Whether or not to document runs')
-    parser.add_argument('-f', '--adjust_free', type=bool, default=False,
-                        help='Whether prices are adjusted freely or incrementally')
+    add_bool_arg(parser, 'batch_agent_calls')
+    add_bool_arg(parser, 'sync_episodes')
+    add_bool_arg(parser, 'document', default=True)
+    add_bool_arg(parser, 'adjust_free', default=True)
     parser.add_argument('-c', '--checkpoint', type=str, default=None,
                         help='Previous checkpoint to load')
 
     args = parser.parse_args()
+    print(args)
 
     experiment = Experiment(agent=args.agent, num_episodes=args.episodes, batch_agent_calls=args.batch_agent_calls,
                             sync_episodes=args.sync_episodes, num_parallel=args.num_parallel,
