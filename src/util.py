86ad8f7099e6eaf5c50cfe89c4b7ebdd58c9555a
JakobGutmann <JakobGutmann@users.noreply.github.com>
2021-07-09 15:03:41 +0200

    add param for global occupancy in reward function

--- src/util.py
+++ src/util.py
@@ -7,30 +7,32 @@ from typing import List, Dict
 import numpy as np
 
 
-def occupancy_reward_function(colours: List[str], current_state: Dict[str, float]):
+def occupancy_reward_function(colours: List[str], current_state: Dict[str, float], global_mode=False):
     """
     Rewards occupancy rates between 75% and 90%. Punishes deviations as the squared deviation from the target range.
     :param current_state:
     :param colours:
+    :param global_mode:
     :return:
     """
     reward = 0
-    for c in colours:
-        if current_state[f'{c}-lot occupancy'] <= 0.825:
-            reward += 1 - (abs(current_state[f'{c}-lot occupancy'] - 0.825) / 0.825) ** 0.4
+    if global_mode:
+        cpz_occupancies = [current_state['overall_occupancy']]
+    else:
+        cpz_occupancies = [current_state[f'{c}-lot occupancy'] for c in colours]
+
+    for val in cpz_occupancies:
+        if val <= 0.825:
+            reward += 1 - (abs(val - 0.825) / 0.825) ** 0.4
         else:
-            value = 1 - (abs(current_state[f'{c}-lot occupancy'] - 0.825) / 0.825) ** 0.4
+            value = 1 - (abs(val - 0.825) / 0.825) ** 0.4
             min_value = 1 - (abs(1 - 0.825) / 0.825) ** 0.4
             max_value = 1 - (abs(0.825 - 0.825) / 0.825) ** 0.4
             max_distance = max_value - min_value
             actual_distance = value - min_value
             reward += actual_distance / max_distance
-        # if 0.75 < current_state[f'{c}-lot occupancy'] < 0.90:
-        #     reward += 0.25
-        # else:
-        #     reward -= 0.25
 
-    return reward / len(colours)
+    return reward / len(cpz_occupancies)
 
 
 def n_cars_reward_function(colours: List[str], current_state: Dict[str, float]):
@@ -70,7 +72,7 @@ def composite_reward_function(colours: List[str], current_state: Dict[str, float
     :param current_state:
     :return:
     """
-    return 0.5 * occupancy_reward_function(colours, current_state) + 0.25 * n_cars_reward_function(
+    return 0.5 * occupancy_reward_function(colours, current_state, global_mode=True) + 0.25 * n_cars_reward_function(
         colours, current_state) + 0.25 * social_reward_function(colours, current_state)
 
 
